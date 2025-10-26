7b6d17475fdb797f33dffff1fe85c6b3e2c1f96e
Michael Landry <506mikey@gmail.com>
2025-10-26 17:53:47 -0300

    Create __init__.py

---
+++ quantum_temple_multiverse/cognition/__init__.py
@@ -0,0 +1,6 @@
+"""
+FILE: quantum_temple_multiverse/cognition/__init__.py
+PURPOSE: Subpackage init for cognition subsystem.
+MATHEMATICAL CORE: N/A
+INTEGRATION POINTS: cognition.stress_harness, cognition.entropic_simulator, cognition.consciousness_metrics
+"""

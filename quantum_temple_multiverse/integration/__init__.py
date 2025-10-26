aee70a18705b203efb9558e80063852b2579ff14
Michael Landry <506mikey@gmail.com>
2025-10-26 17:56:46 -0300

    Create __init__.py

---
+++ quantum_temple_multiverse/integration/__init__.py
@@ -0,0 +1,6 @@
+"""
+FILE: quantum_temple_multiverse/integration/__init__.py
+PURPOSE: Subpackage init for integration subsystem.
+MATHEMATICAL CORE: N/A
+INTEGRATION POINTS: integration.protocol_weaver, integration.emergent_properties
+"""

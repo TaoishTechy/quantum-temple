fc702e9a2f54a8a0971604b6970f5729d2467a01
Michael Landry <506mikey@gmail.com>
2025-10-26 16:23:04 -0300

    Update ghost_mesh.py

--- src/topology/ghost_mesh.py
+++ src/topology/ghost_mesh.py
@@ -1,1 +1,18 @@
+# src/topology/ghost_mesh.py
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import List, Tuple, Optional
 
+@dataclass
+class GhostMesh:
+    n: int
+    cross_links: Optional[List[Tuple[int,int]]] = None
+
+    def neighbors(self) -> List[List[int]]:
+        """Ring + mesh neighbors (undirected)."""
+        nb = [[(i-1) % self.n, (i+1) % self.n] for i in range(self.n)]
+        if self.cross_links:
+            for a,b in self.cross_links:
+                if b not in nb[a]: nb[a].append(b)
+                if a not in nb[b]: nb[b].append(a)
+        return nb

4adafb57af186cdec60af43e534834c17d8d5026
Michael Landry <506mikey@gmail.com>
2025-10-26 17:53:13 -0300

    Create __init__.py

---
+++ quantum_temple_multiverse/multiverse/__init__.py
@@ -0,0 +1,6 @@
+"""
+FILE: quantum_temple_multiverse/multiverse/__init__.py
+PURPOSE: Subpackage init for multiverse subsystem.
+MATHEMATICAL CORE: N/A
+INTEGRATION POINTS: multiverse.core, multiverse.agi_seeds, multiverse.reality_registry
+"""

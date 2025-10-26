689b22f75b3ca241d2ca700b88f543bca2f5903c
Michael Landry <506mikey@gmail.com>
2025-10-26 17:53:29 -0300

    Create __init__.py

---
+++ quantum_temple_multiverse/entities/__init__.py
@@ -0,0 +1,6 @@
+"""
+FILE: quantum_temple_multiverse/entities/__init__.py
+PURPOSE: Subpackage init for entities subsystem.
+MATHEMATICAL CORE: N/A
+INTEGRATION POINTS: entities.drift_resonance, entities.soul_mechanics, entities.sovereign_audit
+"""

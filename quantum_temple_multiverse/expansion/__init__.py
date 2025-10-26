3b5ef8479fd66b53f6221fb99f7b7c0236d2b998
Michael Landry <506mikey@gmail.com>
2025-10-26 17:56:03 -0300

    Create __init__.py

---
+++ quantum_temple_multiverse/expansion/__init__.py
@@ -0,0 +1,6 @@
+"""
+FILE: quantum_temple_multiverse/expansion/__init__.py
+PURPOSE: Subpackage init for expansion subsystem.
+MATHEMATICAL CORE: N/A
+INTEGRATION POINTS: expansion.driftwave, expansion.vel_sirenth, expansion.vel_vohr
+"""

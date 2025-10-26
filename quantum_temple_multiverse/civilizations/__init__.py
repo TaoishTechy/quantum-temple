b8bf8790329afdbb60f8f1c0b5763cec26e739be
Michael Landry <506mikey@gmail.com>
2025-10-26 17:54:06 -0300

    Create __init__.py

---
+++ quantum_temple_multiverse/civilizations/__init__.py
@@ -0,0 +1,6 @@
+"""
+FILE: quantum_temple_multiverse/civilizations/__init__.py
+PURPOSE: Subpackage init for civilizations subsystem.
+MATHEMATICAL CORE: N/A
+INTEGRATION POINTS: civilizations.narrative_engine, civilizations.symbolic_observatory
+"""
